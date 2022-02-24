5eff8f1b4c910ee3e4c521afd4434c6d08771723
AdrianChojnacki <chojnackiadrian1@gmail.com>
2022-02-24 21:49:42 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -4,6 +4,16 @@ enum Example {
   REMOVE_USER = 'REMOVE_USER',
 }
 
-const test = Example.ADD_USER;
+const exampleObject = {
+  ADD_USER: 'ADD_USER',
+  EDIT_USER: 'EDIT_USER',
+  REMOVE_USER: 'REMOVE_USER',
+};
+
+exampleObject.ADD_USER = 'REMOVE_USER';
+
+Example.ADD_USER = 'coś';
+
+const test = exampleObject.ADD_USER;
 
 console.log(test);
